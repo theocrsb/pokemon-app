0d97f0f96d995c79d1682edad49bf08504b0328c
Théo Crosbie <theo_crosbie@yahoo.fr>
2023-03-31 15:12:51 +0200

    ajout @Input dans la directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,23 +1,35 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
   selector: "[pkmnBorderCard]",
 })
 export class BorderCardDirective {
+  private initialColor: string = "#f5f5f5";
+  private defaultColor: string = "#009688";
+  private defaultHeight: number = 180;
+  private PhoneHeight: number = 250;
+
   constructor(private el: ElementRef) {
     this.setHight(180);
     this.setBorder("#f5f5f5");
   }
 
+  @Input("pkmnBorderCard") borderColor: string; // alias
+  @Input() pkmnBorderCard: string; // sans alias
+
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder("#009688");
+    this.setBorder(this.borderColor || this.defaultColor);
   }
   @HostListener("mouseleave") onMouseLeave() {
-    this.setBorder("#f5f5f5");
+    this.setBorder(this.initialColor);
   }
 
   setHight(height: number) {
-    this.el.nativeElement.style.height = `${height}`;
+    if (screen.width == 500) {
+      this.el.nativeElement.style.height = this.defaultHeight + "px";
+    } else {
+      this.el.nativeElement.style.height = this.PhoneHeight + "px";
+    }
   }
 
   setBorder(color: string) {
