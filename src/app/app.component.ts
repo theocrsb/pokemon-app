ab1257a53b13fddb75b229d2c12353a6e4501544
Theocrsb <theo_crosbie@yahoo.fr>
2023-03-31 08:53:38 +0200

    maj avant pull

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: ` <h1>Welcome to {{ pokemonList[0] }}!</h1> `,
+  template: ` <h1>Liste de Pokémons</h1> `,
   styles: ['h1 { text-align : center; }'],
 })
 // implements OnInit pour utiliser ngOnInit()
 export class AppComponent implements OnInit {
   title = 'pokemon-app';
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList = POKEMONS;
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon('Bulbizarre');
+    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemonName: string) {
-    console.log(`Vous avez cliqué sur le pokemon ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
   }
 }
