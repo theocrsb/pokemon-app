47deb11a6e55bbbc0b9f4b99c22d70e355b332b8
Théo Crosbie <theo_crosbie@yahoo.fr>
2023-03-31 14:33:04 +0200

    ngFor pour afficher tous les pokemn + style materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   templateUrl: `./app.component.html`,
-  styles: ['h1 { text-align : center; }'],
+  // styles: ["h1 { text-align : center; }"],
 })
 // implements OnInit pour utiliser ngOnInit()
 export class AppComponent implements OnInit {
-  title = 'pokemon-app';
-  pokemonList = POKEMONS;
+  title = "pokemon-app";
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelect: Pokemon | undefined;
+
   ngOnInit(): void {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    if (pokemon) {
+      console.log(`Vous avez demandé sur le pokemon ${pokemon.name}`);
+      this.pokemonSelect = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokemon qui n'existe pas`);
+      this.pokemonSelect = pokemon;
+    }
   }
 }
