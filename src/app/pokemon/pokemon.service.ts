3fe0f3782f82fc47d1ec5fac85d7627a18b6a1b6
Théo Crosbie <theo_crosbie@yahoo.fr>
2023-04-03 20:28:49 +0200

    refacto pokemon service

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -11,27 +11,21 @@ export class PokemonService {
   getPokemonList(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>("api/pokemons").pipe(
       tap((response) => this.log(response)),
-      catchError((error) => {
-        console.log(error);
-        return of([]);
-      })
+      catchError((error) => this.handleError(error, []))
     );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
     return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
       tap((response) => this.log(response)),
-      catchError((error) => {
-        console.log(error);
-        return of(undefined);
-      })
+      catchError((error) => this.handleError(error, undefined))
     );
   }
 
   private log(response: Pokemon | Pokemon[] | undefined) {
     console.table(response);
   }
-  private handleError(err: Error, errorValue: [] | undefined) {
+  private handleError(err: Error, errorValue: any) {
     console.error(err);
     return of(errorValue);
   }
