a4f2be898b9fbf553338e2bd225ff8c9129eed99
Theocrsb <theo_crosbie@yahoo.fr>
2023-04-04 16:18:01 +0200

    ajout loader

--- src/app/pokemon/loader/loader.component.ts
+++ src/app/pokemon/loader/loader.component.ts
@@ -3,13 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loader',
   template: `
-    <p>
-      loader works!
-    </p>
+    <div class="preloader-wrapper big active">
+      <div class="spinner-layer spinner-blue">
+        <div class="circle-clipper left">
+          <div class="circle"></div>
+        </div>
+        <div class="gap-patch">
+          <div class="circle"></div>
+        </div>
+        <div class="circle-clipper right">
+          <div class="circle"></div>
+        </div>
+      </div>
+    </div>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class LoaderComponent {
-
-}
+export class LoaderComponent {}
