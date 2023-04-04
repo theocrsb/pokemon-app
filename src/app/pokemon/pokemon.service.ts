f1391f0fd14c91ae8d5b70bb40e81dc8d79477d9
Theocrsb <theo_crosbie@yahoo.fr>
2023-04-04 14:11:13 +0200

    generate search pokemon

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -22,6 +22,13 @@ export class PokemonService {
     );
   }
 
+  searchPokemonList(term: string): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>(`api/pokemons/?name=${term}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
+    );
+  }
+
   updatePokemon(pokemon: Pokemon): Observable<Pokemon | null> {
     const httpOption = {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
